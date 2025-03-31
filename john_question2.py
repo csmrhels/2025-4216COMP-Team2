8fc7a7074426dd74746befecd8b5370374654f40
John Harkin <CSMjhark@ljmu.ac.uk>
2025-03-31 19:41:42 +0100

    updated code to work with data

--- john_question2.py
+++ john_question2.py
@@ -1,19 +1,30 @@
 import matplotlib.pyplot as plt 
-import csv 
+import csv
   
 x = [] 
 y = [] 
+
+
+with open('climate_change_dataset_rounded.csv','r') as csvfile:
+    plots = csv.reader(csvfile)
+
+    csv.reader= csv.reader(csvfile) 
+    next(csv.reader) #skips the headings
+
+    for row in plots:
+        x.append(row[0]) #inserts the countries
+        y.append(float(row[7])) #inserts the renewable energy %
+        
   
-with open('climate_change_dataset_rounded.csv','r') as csvfile: 
-    plots = csv.reader(csvfile, delimiter = ',') 
-      
-    for row in plots: 
-        x.append(row[0]) 
-        y.append(float(row[7])) 
-  
-plt.bar(x, y, color = 'g', width = 0.72, label = "renewable energy %") 
+plt.bar(x, y, color = 'r', width = 0.5, label = "renewable energy %") 
+
 plt.xlabel('countires') 
-plt.ylabel('percentage of renewable energy') 
-plt.title('graph showing which country has the highest percentage of renewable energy') 
+plt.ylabel('countries highest percentage of renewable energy') 
+plt.title('graph showing which country has the highest percentage of renewable energy')
 plt.legend() 
+plt.grid(True)
+plt.ylim(34,51)
+plt.yticks(range(34,51,2))
+
+
 plt.show()
