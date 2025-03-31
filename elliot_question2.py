77243f14ceb5296883755cca93ad068711426954
Elliot Charles Highdale <ehighdale12@gmail.com>
2025-03-31 15:38:59 +0100

    Re-upload of new second visualisation

---
+++ elliot_question2.py
@@ -0,0 +1,12 @@
+#Question: Does a countries CO2 Emissions have a direct effect on a rise in sea level?
+
+import matplotlib.pyplot as plt
+import csv
+import pandas as pd
+
+path = "climate_change_dataset_rounded.csv"
+df = pd.read_csv(path)
+
+plt.figure(figsize=(10, 6))
+
+plt.show()
