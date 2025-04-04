7eaab204e6f54bf3fa198d90e33e54a8a77b942d
John Harkin <CSMjhark@ljmu.ac.uk>
2025-04-04 14:52:12 +0100

    updated code

--- john_question1.py
+++ john_question1.py
@@ -18,31 +18,36 @@ with open('climate_change_dataset_rounded.csv','r') as csvfile:
         year = (row[1]) #inserts the years
         sea_level = (float(row[4])) #inserts the sea level rise
 
-        if 298 <= int <= 319:
+        if 296 <= int <= 317:
             file['UK']['x'].append(year)
             file['UK']['y'].append(sea_level)
-        elif 320 <= int < 342:
+        elif 318 <= int <= 339:
             file['USA']['x'].append(year)
             file['USA']['y'].append(sea_level)
-        elif 92 <= int < 113:
+        elif 90 <= int <= 110:
             file['China']['x'].append(year)
             file['China']['y'].append(sea_level)
-        elif 24 <= int < 45:
+        elif 22 <= int <= 42:
             file['Australia']['x'].append(year)
             file['Australia']['y'].append(sea_level)
 
-fig, axs = plt.subplots(2,2)
+fig, axs = plt.subplots(2,2, figsize=(10, 8))
+plt.tight_layout()
 
 axs[0][0].plot(file['UK']['x'], file['UK']['y'])
+axs[0,0].set_ylabel("sea level rise mm")
 axs[0][0].set_title("UK")
 
 axs[0][1].plot(file['USA']['x'], file['USA']['y'])
+axs[0,1].set_ylabel("sea level rise mm")
 axs[0][1].set_title("USA")
 
 axs[1][0].plot(file['China']['x'], file['China']['y'])
+axs[1,0].set_ylabel("sea level rise mm")
 axs[1][0].set_title("China")
 
 axs[1][1].plot(file['Australia']['x'], file['Australia']['y'])
+axs[1,1].set_ylabel("sea level rise mm")
 axs[1][1].set_title("Australia")
 
 plt.show()
