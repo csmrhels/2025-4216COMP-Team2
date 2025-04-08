6ee898e1339759675a4967481f820c0be9d8c8bd
Charlotte Kempster <csmckemp@ljmu.ac.uk>
2025-04-08 10:12:32 +0100

    Completed question2

--- charlotte_question2.py
+++ charlotte_question2.py
@@ -4,15 +4,26 @@ import pandas as pd
 file_path = r"climate_change_dataset_rounded.csv"
 df = pd.read_csv(file_path, encoding="latin1")
 
-meanTempPerCountry = df.groupby("Country")["Avg Temperature (Â°C)"].mean().reset_index()
+# Group the data by 'Country' and calculate the mean of the 'Avg Temperature (°C)' for each country
+meanTempPerCountry = df.groupby("Country")[["Avg Temperature (°C)"]].mean().reset_index()
 
-meanTempPerCountry= meanTempPerCountry.sort_values(by="Avg Temperature (Â°C)", ascending=True)
+# Sort the countries by their average temperature in ascending order
+meanTempPerCountry= meanTempPerCountry.sort_values(by="Avg Temperature (°C)", ascending=True)
 
+# Create the plot and define size of plot
 plt.figure(figsize=(12, 6))
-plt.barh(meanTempPerCountry["Country"], meanTempPerCountry["Avg Temperature (Â°C)"], color="red")
+ 
+# Create a horizontal bar plot showing the average temperature by country
+plt.barh(meanTempPerCountry["Country"], meanTempPerCountry["Avg Temperature (°C)"], color="red")
+
+#Label axis
 plt.xlabel("Average Temperature (°C)")
 plt.ylabel("Country")
+
+#Add tiltle
 plt.title("Average Temperature by Country")
+
+#Add gridlines
 plt.grid(axis="x", linestyle="--", alpha=0.7)
 
 # Show the plot
